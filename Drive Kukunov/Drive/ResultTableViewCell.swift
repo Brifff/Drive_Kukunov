f9f21479b5442f03f316df28b4d11c4225ad6ee4
Ataev.m.N <104029906+Brifff@users.noreply.github.com>
2023-03-20 12:14:16 +0300

    Project init

---
+++ Drive Kukunov/Drive/ResultTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTableViewCell.swift
+//  Drive
+//
+//  Created by Кукунов Олег Дмитриевич on 16/1/2023.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    
+   
+}
